be5a9c27d9688ad159b598f52ebae5d976ec9e54
yezigege <18839136833@163.com>
2020-01-07 21:51:19 +0800

    改写试图函数

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'my_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # 定义模板位置
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
