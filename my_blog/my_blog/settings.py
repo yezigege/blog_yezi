433c95a56f32b1f295bdb73ec2406b914b18ef1a
yezigege <18839136833@163.com>
2020-01-07 23:05:03 +0800

    添加bootstrap改写模板文件

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 新增的自定义app
-    'article',
+    'article'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
