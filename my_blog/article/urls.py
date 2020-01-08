e4d26f01363bbad5a217d3dfc08a4df8622e95f6
yezigege <18839136833@163.com>
2020-01-08 23:16:44 +0800

    编写文章详情页面

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -8,5 +8,7 @@ app_name = 'article'  # 正在部署的应用的名称
 # 此 app 对应的 url 路径
 urlpatterns = [
     # path 函数将 url 映射到视图
-    path('article-list/', views.article_list, name='article-list'),
+    path('article_list/', views.article_list, name='article_list'),
+    # 文章详情
+    path('article_detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
