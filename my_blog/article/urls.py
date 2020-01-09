719940f355f53edeb6e7d729794ce961ea473b13
yezigege <18839136833@163.com>
2020-01-09 22:56:27 +0800

    增加表单类的使用-增加新文章的编写

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('article_list/', views.article_list, name='article_list'),
     # 文章详情
     path('article_detail/<int:id>/', views.article_detail, name='article_detail'),
+    # 写文章
+    path('article_create/', views.article_create, name='article_create'),
 ]
